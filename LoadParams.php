ee5398a7bf585c21c4add7464a73c61c19808ae0
SergeiGulin <gulin-sergei@ya.ru>
2014-09-09 14:52:51 +0400

    Update LoadParams.php

--- LoadParams.php
+++ LoadParams.php
@@ -2,9 +2,22 @@
 namespace sgdot/params;
 
 use yii\base\Component;
+use yii\base\Event;
+use yii\web\Application;
+use yii\helpers\ArrayHelper;
 
 class LoadParams extends Component {
+    public $modelClass;
+    public $attributeKey;
+    public $attributeValue;
     public function init() {
+        $class     = $this->modelClass;
+        $attrKey   = $this->attributeKey;
+        $attrValue = $this->attributeValue;
         
+        $data = $class::find()->all();
+        $settings = ArrayHelper::map($data, $attrKey,$attrValue);
+        \Yii::$app->params = array_merge($settings, \Yii::$app->params);    
+
     }
 }
